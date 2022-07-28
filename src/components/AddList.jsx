511c3011b9ec60cb6795c4d0a39b7d4608d63787
Younes-98 <yenesar98@gmail.com>
2022-07-28 02:05:04 +0200

    Fixed id of each feild

--- src/components/AddList.jsx
+++ src/components/AddList.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { useState } from 'react';
 
 
+
 export default function AddList({showAlert, setShowAlert}) {
 
+    const id = Math.floor(Math.random() * 100000);
     const [feild, setFeild] = useState('')
+
     const [listOffeild, setListOffeild] = useState([])
     
 
@@ -14,13 +17,15 @@ export default function AddList({showAlert, setShowAlert}) {
 
     const Add = () => {
         if(feild !== ''){
-            setListOffeild([...listOffeild, feild])
-            setFeild('')
+            setListOffeild([...listOffeild, {id: id, feild: feild}])
+            // setFeild('')
         } else {
             setShowAlert(true)
             setTimeout(() => setShowAlert(false), 2000);
         }
     }
+
+    console.log(listOffeild)
   return (
     <div className="flex flex-col">
         <div className="flex text-center justify-center">
@@ -30,8 +35,8 @@ export default function AddList({showAlert, setShowAlert}) {
 
         <div>
             {listOffeild.map(feildoflist => (
-                <div className="border-solid bg-accent-content border-2 border-white rounded-lg flex text-center items-center justify-between mt-5 p-3">
-                    <p className="mr-8">{feildoflist}</p>
+                <div key={feildoflist.id} className="border-solid bg-accent-content border-2 border-white rounded-lg flex text-center items-center justify-between mt-5 p-3">
+                    <p className="mr-8">{feildoflist.feild}</p>
                     <div>
                         <button className="inline-flex animate-pulse items-center mr-2 p-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -39,7 +44,7 @@ export default function AddList({showAlert, setShowAlert}) {
                         </svg>
                         </button>
                         <button className="inline-flex animate-pulse items-center p-2 bg-green-600 hover:bg-green-700 text-white text-sm font-medium rounded-md">
-                        <svg className="w-6 h-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 14l9-5-9-5-9 5 9 5z"></path><path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path><path stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path></svg>
+                        <svg className="w-6 h-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 14l9-5-9-5-9 5 9 5z"></path><path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path></svg>
                         </button>
                     </div>  
                 </div>))}
