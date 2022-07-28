6c0b8a47c507040950ee4e5db5785e0f4ea67bb5
Younes-98 <yenesar98@gmail.com>
2022-07-28 15:42:20 +0200

    Fix button delete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [showAlert, setShowAlert] = useState(false)
   return (
     <div className="bg-[#333] text-[#00FFFF] py-8 flex flex-col text-center items-center justify-center min-h-screen h-full">
-      <>{showAlert && <Alert />}</>
+      <>{showAlert && <Alert message={'Le champ est vide!'}/>}</>
       <h1 className="text-3xl mt-5 mb-5">To Do List React App</h1>
       <AddList showAlert={showAlert} setShowAlert={setShowAlert}/>
     </div>
--- src/components/AddList.jsx
+++ src/components/AddList.jsx
@@ -9,7 +9,7 @@ export default function AddList({showAlert, setShowAlert}) {
     const [feild, setFeild] = useState('')
 
     const [listOffeild, setListOffeild] = useState([])
-    
+
 
     const change = (e) => {
         setFeild(e.target.value)
@@ -18,11 +18,16 @@ export default function AddList({showAlert, setShowAlert}) {
     const Add = () => {
         if(feild !== ''){
             setListOffeild([...listOffeild, {id: id, feild: feild}])
-            // setFeild('')
+            setFeild('')
         } else {
             setShowAlert(true)
             setTimeout(() => setShowAlert(false), 2000);
         }
+    }
+
+    const Delete = (feild, id) => {
+        (window.confirm(`Vous voullez vraiment supprimer ${feild} !`) === true) && (
+            setListOffeild(listOffeild.filter(item => item.id !== id))) 
     }
 
     console.log(listOffeild)
@@ -38,13 +43,13 @@ export default function AddList({showAlert, setShowAlert}) {
                 <div key={feildoflist.id} className="border-solid bg-accent-content border-2 border-white rounded-lg flex text-center items-center justify-between mt-5 p-3">
                     <p className="mr-8">{feildoflist.feild}</p>
                     <div>
-                        <button className="inline-flex animate-pulse items-center mr-2 p-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                        </svg>
+                        <button onClick={() => Delete(feildoflist.feild, feildoflist.id)} className="inline-flex animate-pulse items-center mr-2 p-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
                         </button>
                         <button className="inline-flex animate-pulse items-center p-2 bg-green-600 hover:bg-green-700 text-white text-sm font-medium rounded-md">
-                        <svg className="w-6 h-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 14l9-5-9-5-9 5 9 5z"></path><path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path></svg>
+                            <svg className="w-6 h-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 14l9-5-9-5-9 5 9 5z"></path><path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path></svg>
                         </button>
                     </div>  
                 </div>))}
